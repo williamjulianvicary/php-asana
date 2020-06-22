de79636cd953ca8cab88884fa354a5029237270f
William Vicary <will@3whitehats.com>
2020-06-22 15:29:04 +0100

    Persist the sync token during pagination.

--- src/Asana/Iterator/CollectionPageIterator.php
+++ src/Asana/Iterator/CollectionPageIterator.php
@@ -6,6 +6,8 @@ use Asana\Iterator\PageIterator;
 
 class CollectionPageIterator extends PageIterator
 {
+    public $sync = null;
+
     protected function getInitial()
     {
         return $this->client->get($this->path, $this->query, $this->options);
@@ -19,6 +21,8 @@ class CollectionPageIterator extends PageIterator
 
     protected function getContinuation($result)
     {
+        $this->sync = $result->sync ?? null;
+
         return isset($result->next_page) ? $result->next_page : null;
     }
 }
